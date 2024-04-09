c54e13c5baf04a0e098fa45a5f820449ecf15aec
Abdul Samad <as765722@gmail.com>
2024-04-09 09:59:11 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todo-app/',
   plugins: [react()],
   define: {
     '__DEV__': process.env.NODE_ENV !== 'production',
